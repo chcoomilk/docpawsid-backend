edc9fbbe5319f275f8fd0f52f0f2a4a426e4c707
klaustinw <klaustin.wijaya@gmail.com>
2021-07-15 17:41:45 +0700

    update branch

--- src/routes/vaccines/helper.ts
+++ src/routes/vaccines/helper.ts
@@ -1,7 +1,7 @@
 import { VaccineHistory } from "@prisma/client";
 
 export const validate_vaccine_input = (history: VaccineHistory): string | null => {
-  // const a = Date.parse(String(history.date_administered));
-  // if (a)
+  // const  = Date.parse(String(history.date_administered));
+  // if ()
   return null;
 };
